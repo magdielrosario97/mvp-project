f174252f4a2f70a720650021c27e6000eda0badf
magdielrosario97 <magdielrosario97@gmail.com>
2022-05-24 16:19:38 -0500

    Initial deployment to track heroku compability

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const pool = require("./database/connection");
 
-app.use(express.static("client-side"));
+app.use(express.static("public"));
 app.use(express.json());
 
 // get the main blog page with all posts and comments
